ef89238955f83b851309706bfe012d841899f071
SakariasRE <Sakarias.Eriksson@iths.se>
2026-08-15 16:46:12 +0200

    sell items

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import { useEffect, useState } from 'react';
-import Sidebar from './components/sidebar';
-import Header from './components/header';
-import Market from './pages/market';
-import Inventory from './pages/inventory';
+import { useEffect, useState } from "react";
+import Sidebar from "./components/sidebar";
+import Header from "./components/header";
+import Inventory from "./pages/inventory";
+import SellItem from "./pages/sellItem";
+import Market from "./pages/market";
 
 function App() {
-    const [balance, setBalance] = useState(() => {
-        const savedBalance = localStorage.getItem('balance');
-        return savedBalance ? Number(savedBalance) : 500000;
-    });
+  const [balance, setBalance] = useState(() => {
+    const savedBalance = localStorage.getItem("balance");
+    return savedBalance ? Number(savedBalance) : 500000;
+  });
 
-    useEffect(() => {
-        localStorage.setItem('balance', balance.toString());
-    }, [balance]);
+  const [currentPage, setCurrentPage] = useState("inventory");
 
-    return (
-        <div className="flex min-h-screen bg-neutral-950 text-neutral-100">
-            <Sidebar />
+  useEffect(() => {
+    localStorage.setItem("balance", balance.toString());
+  }, [balance]);
 
-            <div className="flex flex-1 flex-col">
-                <Header
-                balance={balance}
-                setBalance={setBalance}
-                />
+  return (
+    <div className="flex min-h-screen bg-neutral-950 text-neutral-100">
+      <Sidebar
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
 
-                
-                <Inventory />
-            </div>
-        </div>
-    );
+      <div className="flex flex-1 flex-col">
+        <Header
+          balance={balance}
+          setBalance={setBalance}
+        />
+
+        {currentPage === "marketplace" && <Market />}
+        {currentPage === "inventory" && <Inventory />}
+        {currentPage === "sell" && <SellItem />}
+      </div>
+    </div>
+  );
 }
 
 export default App;
